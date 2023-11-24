5b62d63620a35743e494a6de60c170dd0b49ce1d
sanlamamba <contact@sanlamamba.com>
2023-11-24 03:10:46 +0100

    feat: :sparkles: implement authentication and user management in Express app
    - Installed and integrated bcrypt and jsonwebtoken for secure authentication. - Configured Express app with middleware for JSON and URL-encoded body parsing, and CORS support. - Added new authentication features including login, registration, JWT verification, and token refresh functionality. - Created a Mongoose schema-based model for user data management. - Enhanced the user model with custom fields including a 'watchlist'. - Established new routes for handling user registration, login, and token refresh requests.

--- coin_count_api/src/controllers/users.ts
+++ coin_count_api/src/controllers/users.ts
@@ -1,58 +1,86 @@
 import { Request, Response } from 'express';
-import User from '../models/users';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from "../models/users"
+const JWT_SECRET = process.env.JWT_SECRET || 'SECRET'; // Use an environment variable
 
-// CREATE
-export const createUser = async (req: Request, res: Response) => {
+const generateJwt = (userId: string): string => {
+    return jwt.sign({ id: userId }, JWT_SECRET, {
+        expiresIn: "7d"
+    });
+};
+const verifyJwt = (token: string): { userId: string } | null => {
     try {
-        const user = new User(req.body);
+        const decoded = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
+        return { userId: decoded.id }; // Make sure this key matches what's set in generateJwt
+    } catch (error) {
+        return null; // Handle the error as needed
+    }
+};
+
+// Registration function
+export const register = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        // if (!req.body.username || !req.body.email || !req.body.password) res.status(400).json({ message: 'Username, email, and password are required' });
+        const { username, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+        // if (userExists) {
+        //     return res.status(400).json({ message: 'User already exists' });
+        // }
+        const user = new User({ username, email, password });
         await user.save();
-        res.status(200).json(user.toJSON());
-    } catch (err: any) {
-        res.status(400).json({ message: err.message });
+        user.password = undefined;
+        const token = generateJwt(user._id);
+        return res.status(201).json({ message: 'User created successfully', token, user });
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error', error: error });
     }
 };
 
-// READ
-export const getUsers = async (req: Request, res: Response) => {
+// Login function
+export const login = async (req: Request, res: Response): Promise<Response> => {
     try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err: any) {
-        res.status(500).json({ message: err.message });
+        if (!req.body.email || !req.body.password) res.status(400).json({ message: 'Username, email, and password are required' });
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        // Check password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        // remove user.password;
+        user.password = undefined;
+        const token = generateJwt(user._id);
+        return res.json({ message: 'Logged in successfully', token, user });
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error' });
     }
 };
 
-export const getUserById = async (req: Request, res: Response) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) throw new Error('User not found');
-        res.json(user);
-    } catch (err: any) {
-        res.status(404).json({ message: err.message });
-    }
-};
 
-// UPDATE
-export const updateUser = async (req: Request, res: Response) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) throw new Error('User not found');
-        Object.assign(user, req.body);
-        await user.save();
-        res.json(user);
-    } catch (err: any) {
-        res.status(404).json({ message: err.message });
+export const refreshToken = async (req: Request, res: Response): Promise<Response> => {
+    const token = req.headers.authorization?.split(' ')[1]; // Assuming token is sent in the Authorization header
+    console.log(token);
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
     }
-};
-
-// DELETE
-export const deleteUser = async (req: Request, res: Response) => {
+    const decoded = verifyJwt(token);
+    if (!decoded) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
     try {
-        const user = await User.findById(req.params.id);
-        if (!user) throw new Error('User not found');
-        // await user.remove();
-        res.json({ message: 'User deleted' });
-    } catch (err: any) {
-        res.status(404).json({ message: err.message });
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const newToken = generateJwt(user._id);
+        user.password = undefined;
+        return res.json({ message: 'Token refreshed successfully', token: newToken, user });
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error' });
     }
 };
