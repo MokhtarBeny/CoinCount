903fe57177b68bb998199076503d4b3efae04cf5
Quentin Drcke <82572373+quentin-derycke@users.noreply.github.com>
2023-12-05 16:18:11 +0100

    fix:  json cryptoDatalist

--- coin_count_api/src/routes/cryptos.ts
+++ coin_count_api/src/routes/cryptos.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 const router = express.Router();
 
-const data = {}
+
 const ENDPOINT: string = "cryptos";
 const controller = require(`../controllers/${ENDPOINT}`);
 
 
-router.get(`/${ENDPOINT}/`, (req, res) => {
-    router.get("/cryptos", controller.getCryptosList)
-});
+router.get(`/${ENDPOINT}/`, controller.getCryptosList);
+
 module.exports = router;
