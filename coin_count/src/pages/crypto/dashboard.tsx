f7275dd44ed369ec5aca3326dd038ddfb3eb4f2d
Lamamba <contact@sanlamamba.com>
2024-01-05 00:53:11 +0100

    feat: Adding watchlist logic to the fronted

--- coin_count/src/pages/crypto/dashboard.tsx
+++ coin_count/src/pages/crypto/dashboard.tsx
@@ -1,36 +1,108 @@
 // src/pages/dashboard.tsx
-import React, { useEffect, useState } from 'react';
-import axiosInstance from '@/utils/axios/axiosConfig';
+import React, { useEffect, useState } from "react";
+import axiosInstance from "@/utils/axios/axiosConfig";
+import { EyeFilled, StarFilled, StarOutlined } from "@ant-design/icons";
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+  import Link from "next/link";
 
 const DashboardPage: React.FC = () => {
   const [coins, setCoins] = useState([]);
+  const { auth } = useSelector((state: any) => state);
+  const dispatch = useDispatch();
+  const [watchlist, setWatchlist] = useState<any[]>([]);
+  const [canReqFav, setCanReqFav] = useState(true);
+
+
+  const generateTrendData = (changePercent, length = 5) => {
+    let trendData = [50];
+    for (let i = 1; i < length; i++) {
+      trendData.push(
+        trendData[i - 1] + (changePercent > 0 ? 1 : -1) * Math.random() * 10
+      );
+    }
+    return trendData;
+  };
+  console.log(auth)
+  const addToWatchList = async (coin: any) => {
+    if(auth.token  === "" || auth.token === null) {
+      if(!canReqFav) return;
+      toast.info("Please login to add to watchlist");
+      setCanReqFav(false);
+      setTimeout(() => {
+        setCanReqFav(true);
+      }, 5000);
+      return;
+    }
+    console.log(coin);
+    try {
+      const response = await axiosInstance.get(
+        "/auth/watchlist/" + coin._id,
+        coin._id
+      );
+      if (response.status === 200) {
+        setWatchlist(response.data.user.watchlist);
+        toast.success("Ajouté à la watchlist");
+      }
+    } catch (error) {
+      toast.error("Erreur lors de l'ajout à la watchlist");
+    }
+  };
+
+  const removeFromWatchlist = async (coin: any) => {
+    try {
+      const response = await axiosInstance.delete(
+        "/auth/watchlist/" + coin._id,
+        coin._id
+      );
+      if (response.status === 200) {
+        setWatchlist(response.data.user.watchlist);
+        toast.success("Retiré de la watchlist");
+      }
+    } catch (error) {
+      toast.error("Erreur lors du retrait de la watchlist");
+    }
+  }
+  
 
   useEffect(() => {
-    axiosInstance.get('/cryptos').then((response) => {
-      setCoins(response.data);
-    }).catch((error) => {
-      console.error("Error fetching crypto data:", error);
-    });
+    axiosInstance
+      .get("/cryptos")
+      .then((response) => {
+        setCoins(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching crypto data:", error);
+      });
   }, []);
 
-  // Formate le pourcentage et détermine la classe de couleur
   const formatChangePercent = (percent: string) => {
     const change = parseFloat(percent);
-    const className =
-      change < 0 ? 'text-red-500' : 'text-green-500';
-    return (
-      <span className={className}>
-        {change.toFixed(2)}%
-      </span>
-    );
+    const className = change < 0 ? "text-red-500" : "text-green-500";
+    return <span className={className}>{change.toFixed(2)}%</span>;
   };
+
+  useEffect(() => {
+    const fetchWatchlist = async () => {
+      try {
+        const response = await axiosInstance.get("/auth/watchlist");
+        setWatchlist(response.data);
+      } catch (error) {
+        console.error("Error fetching watchlist:", error);
+      }
+    };
+    fetchWatchlist();
+  }, []);
 
   return (
     <div className="container mx-auto px-4 sm:px-8">
       <div className="py-8">
         <div>
-          <h2 className="text-2xl font-semibold leading-tight">Tableau de bord des cryptomonnaies</h2>
+          <h2 className="text-2xl font-semibold leading-tight">
+            Tableau de bord des cryptomonnaies
+          </h2>
         </div>
         <div className="my-2 flex sm:flex-row flex-col">
           {/* Ici, vous pouvez ajouter des boutons de filtrage ou de tri si nécessaire */}
@@ -60,6 +132,12 @@ const DashboardPage: React.FC = () => {
                 <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                   Sparkline
                 </th>
+                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  Favorite
+                </th>
+                <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  View
+                </th>
                 {/* ... autres en-têtes de colonnes comme 1h, 7d, etc ... */}
               </tr>
             </thead>
@@ -74,7 +152,9 @@ const DashboardPage: React.FC = () => {
                   <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                     <div className="flex items-center">
                       <div className="ml-3">
-                        <p className="text-gray-900 whitespace-no-wrap font-bold"> {/* Nom de la cryptomonnaie en gras */}
+                        <p className="text-gray-900 whitespace-no-wrap font-bold">
+                          {" "}
+                          {/* Nom de la cryptomonnaie en gras */}
                           {coin.name} {coin.symbol}
                         </p>
                       </div>
@@ -82,7 +162,8 @@ const DashboardPage: React.FC = () => {
                   </td>
                   <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                     <p className="text-gray-900 whitespace-no-wrap">
-                      ${parseFloat(coin.priceUsd).toFixed(2)} {/* Prix avec deux décimales */}
+                      ${parseFloat(coin.priceUsd).toFixed(2)}{" "}
+                      {/* Prix avec deux décimales */}
                     </p>
                   </td>
                   <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -90,20 +171,45 @@ const DashboardPage: React.FC = () => {
                   </td>
                   <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                     <p className="text-gray-900 whitespace-no-wrap">
-                      ${parseInt(coin.volumeUsd24Hr).toLocaleString()} {/* Volume avec séparateurs de milliers */}
+                      ${parseInt(coin.volumeUsd24Hr).toLocaleString()}{" "}
+                      {/* Volume avec séparateurs de milliers */}
                     </p>
                   </td>
                   <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                     <p className="text-gray-900 whitespace-no-wrap">
-                      ${parseInt(coin.marketCapUsd).toLocaleString()} {/* Market Cap avec séparateurs de milliers */}
+                      ${parseInt(coin.marketCapUsd).toLocaleString()}{" "}
+                      {/* Market Cap avec séparateurs de milliers */}
                     </p>
                   </td>
                   <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    <p className="text-gray-900 whitespace-no-wrap">
-                      sparkline to do 
-                    </p>
+                    <Sparklines data={generateTrendData(coin.changePercent24Hr)} height={20} width={100}>
+                      <SparklinesLine color={coin.changePercent24Hr >= 0 ? "green" : "red"} />
+                    </Sparklines>
                   </td>
-                  {/* ... autres cellules pour les données de 1h, 7d, etc ... */}
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-large">
+                    {watchlist.find((c) => c === coin._id) ? (
+                      <button
+                        className="btn btn-primary px-3 "
+                        onClick={() => removeFromWatchlist(coin)}
+                      >
+                        <StarFilled className="text-yellow-500" />
+                      </button>
+                    ) : (
+                      <button
+                        className="btn btn-primary px-3 "
+                        onClick={() => addToWatchList(coin)}
+                      >
+                        <StarOutlined className="text-yellow-500" />
+                      </button>
+                    )}
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-large">
+                    
+                    <Link href={`/crypto/${coin.id}`}
+                      className="btn btn-primary px-3">
+                        <EyeFilled />
+                      </Link>
+                  </td>
                 </tr>
               ))}
             </tbody>
