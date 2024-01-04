b535146ceb6c461c297244883357b5b0dad8bdc6
Lamamba <contact@sanlamamba.com>
2024-01-05 00:49:44 +0100

    feat: Added a logo and customize the sign up call to action so that when user is logged it changes

--- coin_count/src/components/Navbar/index.tsx
+++ coin_count/src/components/Navbar/index.tsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from "react";
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  Chip,
+  Avatar,
+} from "@nextui-org/react";
 import SearchBar from "../searchbar";
-
-
+import { useSelector } from "react-redux";
+import Image from "next/image";
 
 export default function _Navbar() {
-
+  const { auth } = useSelector((state: any) => state);
+  console.log(auth);
   const [cryptos, setCryptos] = useState([]);
 
   useEffect(() => {
     // Fonction pour récupérer les données des cryptomonnaies
     const fetchCryptos = async () => {
       try {
-        const response = await fetch('/api/cryptos'); // Ajustez l'URL en fonction de votre configuration d'API
+        const response = await fetch("/api/cryptos"); // Ajustez l'URL en fonction de votre configuration d'API
         const data = await response.json();
         setCryptos(data); // Supposons que l'API renvoie directement un tableau
       } catch (error) {
-        console.error('Erreur lors de la récupération des données de cryptomonnaie:', error);
+        console.error(
+          "Erreur lors de la récupération des données de cryptomonnaie:",
+          error
+        );
       }
     };
 
@@ -26,10 +39,15 @@ export default function _Navbar() {
 
   return (
     <Navbar isBordered isBlurred={false}>
-      <NavbarBrand>       
-       
-         <p className="font-bold text-inherit">Coin Count</p>
-      
+      <NavbarBrand>
+        <div className="flex items-center gap-2">
+          <Link href="/">
+
+          <Image src="/assets/logo.png" width={40} height={40} alt="coin count logo" />
+          <p className="font-bold text-inherit"
+          >Coin Count</p>
+          </Link>
+        </div>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4 " justify="center">
         <NavbarItem>
@@ -49,19 +67,42 @@ export default function _Navbar() {
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="/login">Login</Link>
-        </NavbarItem>
+        {auth.token == "" || auth.token == null ? (
+          <>
+            <NavbarItem className="hidden lg:flex">
+              <Link href="/login">Login</Link>
+            </NavbarItem>
+
+            <NavbarItem>
+              <Button as={Link} color="primary" href="/register" variant="flat">
+                Sign Up
+              </Button>
+            </NavbarItem>
+          </>
+        ) : (
+          <>
+            <Link href="/profile">
+              <a>
+                <Chip
+                  variant="flat"
+                  avatar={
+                    <Avatar
+                      size="sm"
+                      name={auth.user.username[0].toUpperCase()}
+                    />
+                  }
+                >
+                  Welcome, {auth.user.username.split(" ")[0]}
+                </Chip>
+              </a>
+            </Link>
+          </>
+        )}
+
         <NavbarItem>
-          <Button as={Link} color="primary" href="/register" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <SearchBar/>
+          <SearchBar />
         </NavbarItem>
       </NavbarContent>
-      
     </Navbar>
   );
 }
